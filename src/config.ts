6bb8f88a6b0d167b42ed5e8cebe538cca540e3cf
Chad1101 <chad30400@gmail.com>
2024-04-27 18:45:42 +0800

    deploy test

---
+++ src/config.ts
@@ -0,0 +1,72 @@
+import type {
+  LicenseConfig,
+  NavBarConfig,
+  ProfileConfig,
+  SiteConfig,
+} from './types/config'
+import { LinkPreset } from './types/config'
+
+
+
+export const siteConfig: SiteConfig = {
+  //左上角首頁icon旁的Text
+  title: 'Fuwari',
+  subtitle: 'Demo Site',
+  lang: 'zh',
+  themeHue: 250,
+  // 上方橫幅
+  banner: {
+    enable: true,
+    src: 'assets/images/2gungirls.jpg',
+  },
+  // favicon - 網站上方分頁的圖示
+  favicon: [    // Leave this array empty to use the default favicon
+    {
+      src: '/favicon/favicon-dark-192.png',    // Path of the favicon, relative to the /public directory
+      //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      //sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
+  ]
+}
+
+export const navBarConfig: NavBarConfig = {
+  links: [
+    LinkPreset.Home,
+    LinkPreset.Archive,
+    LinkPreset.About,
+    {
+      name: 'GitHub',
+      url: 'https://github.com/saicaca/fuwari',
+      external: true,
+    },
+  ],
+}
+
+export const profileConfig: ProfileConfig = {
+  avatar: 'assets/images/demo-avatar.png',
+  name: 'Lorem Ipsum',
+  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  links: [
+    {
+      name: 'Twitter',
+      icon: 'fa6-brands:twitter',
+      url: 'https://twitter.com',
+    },
+    {
+      name: 'Steam',
+      icon: 'fa6-brands:steam',
+      url: 'https://store.steampowered.com',
+    },
+    {
+      name: 'GitHub',
+      icon: 'fa6-brands:github',
+      url: 'https://github.com/saicaca/fuwari',
+    },
+  ],
+}
+
+export const licenseConfig: LicenseConfig = {
+  enable: true,
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+}
