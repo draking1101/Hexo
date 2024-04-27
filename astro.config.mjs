60e251b00314ecd1e6b76a89cbbe6dc5b3c2028b
Chad1101 <chad30400@gmail.com>
2024-04-27 18:56:49 +0800

    adjust site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://github.com/draking1101/draking1101.github.io",
   base: "/",
   integrations: [
     tailwind(),
